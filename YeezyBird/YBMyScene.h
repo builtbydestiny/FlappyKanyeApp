53fc1ee70f11f45c17868b03a8bf6e438105bbdb
Destiny Makuyana <destiny.makuyana@gmail.com>
2014-08-02 13:27:08 +0100

    Initial Commit

---
+++ YeezyBird/YBMyScene.h
@@ -0,0 +1,13 @@
+//
+//  YBMyScene.h
+//  YeezyBird
+//
+
+//  Copyright (c) 2014 Builtby Destiny. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface YBMyScene : SKScene
+
+@end
---
+++ YeezyBird/YBViewController.m
@@ -0,0 +1,51 @@
+//
+//  YBViewController.m
+//  YeezyBird
+//
+//  Created by APPENGINE on 02/08/2014.
+//  Copyright (c) 2014 Builtby Destiny. All rights reserved.
+//
+
+#import "YBViewController.h"
+#import "YBMyScene.h"
+
+@implementation YBViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Configure the view.
+    SKView * skView = (SKView *)self.view;
+    skView.showsFPS = YES;
+    skView.showsNodeCount = YES;
+    
+    // Create and configure the scene.
+    SKScene * scene = [YBMyScene sceneWithSize:skView.bounds.size];
+    scene.scaleMode = SKSceneScaleModeAspectFill;
+    
+    // Present the scene.
+    [skView presentScene:scene];
+}
+
+- (BOOL)shouldAutorotate
+{
+    return YES;
+}
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return UIInterfaceOrientationMaskAllButUpsideDown;
+    } else {
+        return UIInterfaceOrientationMaskAll;
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+@end
